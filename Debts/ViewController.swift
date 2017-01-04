6425cb4029137c19b1a22e3662919644f6b72baf
blogofcode <blogofcode@gmail.com>
2017-01-04 13:16:00 +0530

    Initial Commit

---
+++ Debts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Debts
+//
+//  Created by Sai Krishna on 04/01/17.
+//  Copyright © 2017 Sai Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
